a2e023f2c7a0581846c207fcfa4e7bf39c4970d4
LiorShur <96306484+LiorShur@users.noreply.github.com>
2025-05-21 17:04:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -130,43 +130,43 @@ const allSessions = await dbGetAll(STORE_NAMES.SESSIONS);
 
 
 
-function setControlButtonsEnabled(enabled) {
-  const idsToDisable = [
-    "startBtn",
-    "resetBtn",
-    "prepareAndExportBtn",
-    "exportAllRoutesBtn",
-    "exportDataBtn",
-    "exportPDFBtn",
-    "exportGPXBtn",
-    "toggleArchivePanelBtn",
-    "clearArchiveBtnBtn",
-    "closeHistoryBtn",
-    "clearAllSessionsBtn",
-    "clearAllAppDataBtn",
-    "loadSessionBtn",
-  ];
+// function setControlButtonsEnabled(enabled) {
+//   const idsToDisable = [
+//     "startBtn",
+//     "resetBtn",
+//     "prepareAndExportBtn",
+//     "exportAllRoutesBtn",
+//     "exportDataBtn",
+//     "exportPDFBtn",
+//     "exportGPXBtn",
+//     "toggleArchivePanelBtn",
+//     "clearArchiveBtnBtn",
+//     "closeHistoryBtn",
+//     "clearAllSessionsBtn",
+//     "clearAllAppDataBtn",
+//     "loadSessionBtn",
+//   ];
 
-  idsToDisable.forEach(id => {
-    const el = document.getElementById(id);
-    if (el) {
-      el.disabled = !enabled;
-      el.style.opacity = enabled ? "1" : "0.5";
-      el.style.pointerEvents = enabled ? "auto" : "none";
-    }
-  });
-}
+//   idsToDisable.forEach(id => {
+//     const el = document.getElementById(id);
+//     if (el) {
+//       el.disabled = !enabled;
+//       el.style.opacity = enabled ? "1" : "0.5";
+//       el.style.pointerEvents = enabled ? "auto" : "none";
+//     }
+//   });
+// }
 
 
-function setTrackingButtonsEnabled(enabled) {
-  const startBtn = document.getElementById("startBtn");
-  const pauseBtn = document.getElementById("pauseBtn");
-  const stopBtn = document.getElementById("stopBtn");
+// function setTrackingButtonsEnabled(enabled) {
+//   const startBtn = document.getElementById("startBtn");
+//   const pauseBtn = document.getElementById("pauseBtn");
+//   const stopBtn = document.getElementById("stopBtn");
 
-  if (startBtn) startBtn.disabled = !enabled;
-  if (pauseBtn) pauseBtn.disabled = !enabled;
-  if (stopBtn) stopBtn.disabled = !enabled;
-}
+//   if (startBtn) startBtn.disabled = !enabled;
+//   if (pauseBtn) pauseBtn.disabled = !enabled;
+//   if (stopBtn) stopBtn.disabled = !enabled;
+// }
 
 const noteIcon = L.divIcon({
   className: 'custom-icon note-icon',
